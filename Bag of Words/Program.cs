f240bd34517e65156d1ad9887796fddfe5010523
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-02-26 19:00:24 +0200

    Add files via upload

---
+++ Bag of Words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Wintellect.PowerCollections;
+namespace Bag_of_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
