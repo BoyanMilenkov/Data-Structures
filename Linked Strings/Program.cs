062aadc10cd13352c6671c90b867585da3c9c579
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-02-26 18:21:51 +0200

    Add files via upload

---
+++ Linked Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            string firstW = Console.ReadLine();
+            string secondW = Console.ReadLine();
+            string thirdW = Console.ReadLine();
+            string fourthW = Console.ReadLine();
+            list.AddFirst(firstW);
+            list.AddLast(secondW);
+            list.AddAfter(list.First, thirdW);
+            list.AddBefore(list.Last, fourthW);
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
