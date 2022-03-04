e4205f03f0ea79e8fa4c4d6638d4b94f5d96c03c
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-03-04 10:18:13 +0200

    Add files via upload

---
+++ HeapOfNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using MoreComplexDataStructures;
+using System;
+
+namespace HeapOfNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxHeap<string> heap = new MaxHeap<string>();
+            heap.Insert("Pesho");
+            heap.Insert("Kiro");
+            heap.Insert("Asen");
+            heap.Insert("Miro");
+
+            while (heap.Count > 0)
+            {
+                Console.WriteLine(heap.ExtractMax());
+            }
+        }
+    }
+}
